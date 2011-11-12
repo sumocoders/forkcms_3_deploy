bad9180538485ce8ebbf2c6c31005b659744093b
Tijs Verkoyen <tijs@verkoyen.eu>
2011-11-12 17:35:07 +0100

    Changed the path to reflect the folderstructure-changes

--- lib/forkcms_3_deploy/forkcms.rb
+++ lib/forkcms_3_deploy/forkcms.rb
@@ -20,22 +20,22 @@ configuration.load do
 		task :clear_cached do
 			# remove frontend cached data
 			run %{
-				rm -rf #{current_path}/default_www/frontend/cache/cached_templates/* &&
-				rm -rf #{current_path}/default_www/frontend/cache/locale/* &&
-				rm -rf #{current_path}/default_www/frontend/cache/minified_css/* &&
-				rm -rf #{current_path}/default_www/frontend/cache/minified_js/* &&
-				rm -rf #{current_path}/default_www/frontend/cache/navigation/* &&
-				rm -rf #{current_path}/default_www/frontend/cache/statistics/* &&
-				rm -rf #{current_path}/default_www/frontend/cache/templates/*
+				rm -rf #{current_path}/frontend/cache/cached_templates/* &&
+				rm -rf #{current_path}/frontend/cache/locale/* &&
+				rm -rf #{current_path}/frontend/cache/minified_css/* &&
+				rm -rf #{current_path}/frontend/cache/minified_js/* &&
+				rm -rf #{current_path}/frontend/cache/navigation/* &&
+				rm -rf #{current_path}/frontend/cache/statistics/* &&
+				rm -rf #{current_path}/frontend/cache/templates/*
 			}
 
 			# remove backend cached data
 			run %{
-				rm -rf #{current_path}/default_www/backend/cache/analytics/* &&
-				rm -rf #{current_path}/default_www/backend/cache/cronjobs/* &&
-				rm -rf #{current_path}/default_www/backend/cache/locale/* &&
-				rm -rf #{current_path}/default_www/backend/cache/mailmotor/* &&
-				rm -rf #{current_path}/default_www/backend/cache/templates/*
+				rm -rf #{current_path}/backend/cache/analytics/* &&
+				rm -rf #{current_path}/backend/cache/cronjobs/* &&
+				rm -rf #{current_path}/backend/cache/locale/* &&
+				rm -rf #{current_path}/backend/cache/mailmotor/* &&
+				rm -rf #{current_path}/backend/cache/templates/*
 			}
 		end
 
@@ -58,8 +58,8 @@ configuration.load do
 
 			# create dirs
 			run %{
-				mkdir -p #{release_path}/default_www/frontend/cache/config &&
-				mkdir -p #{release_path}/default_www/backend/cache/config
+				mkdir -p #{release_path}/frontend/cache/config &&
+				mkdir -p #{release_path}/backend/cache/config
 			}
 
 			# symlink the globals
@@ -67,36 +67,36 @@ configuration.load do
 				ln -sf #{shared_path}/config/library/globals_backend.php #{release_path}/library/globals_backend.php &&
 				ln -sf #{shared_path}/config/library/globals_frontend.php #{release_path}/library/globals_frontend.php &&
 				ln -sf #{shared_path}/config/library/globals.php #{release_path}/library/globals.php &&
-				ln -sf #{shared_path}/config/frontend/config.php #{release_path}/default_www/frontend/cache/config/config.php &&
-				ln -sf #{shared_path}/config/backend/config.php #{release_path}/default_www/backend/cache/config/config.php
+				ln -sf #{shared_path}/config/frontend/config.php #{release_path}/frontend/cache/config/config.php &&
+				ln -sf #{shared_path}/config/backend/config.php #{release_path}/backend/cache/config/config.php
 			}
 		end
 
 		desc 'link the document root to the current/default_www-folder'
 		task :link_document_root do
 			# create symlink for document_root if it doesn't exists
-			documentRootExists = capture("if [ ! -e #{document_root} ]; then ln -sf #{current_path}/default_www #{document_root}; echo 'no'; fi").chomp
+			documentRootExists = capture("if [ ! -e #{document_root} ]; then ln -sf #{current_path} #{document_root}; echo 'no'; fi").chomp
 
 			unless documentRootExists == 'no'
 				warn "Warning: Document root (#{document_root}) already exists"
 				warn 'to link it to the Fork deploy issue the following command:'
-				warn '	ln -sf #{current_path}/default_www #{document_root}'
+				warn '	ln -sf #{current_path} #{document_root}'
 			end 
 		end	
 
 		desc 'Create needed symlinks'
 		task :link_files do
 			# get the list of folders in /frontend/files
-			folders = capture("ls -1 #{release_path}/default_www/frontend/files").split(/\r?\n/)
+			folders = capture("ls -1 #{release_path}/frontend/files").split(/\r?\n/)
 
 			# loop the folders
 			folders.each do |folder|
 				# copy them to the shared path, remove them from the release and symlink them
 				run %{
 				  mkdir -p #{shared_path}/files/#{folder} &&
-					cp -r #{release_path}/default_www/frontend/files/#{folder} #{shared_path}/files/ &&
-					rm -rf #{release_path}/default_www/frontend/files/#{folder} &&
-					ln -s #{shared_path}/files/#{folder} #{release_path}/default_www/frontend/files/#{folder}
+					cp -r #{release_path}/frontend/files/#{folder} #{shared_path}/files/ &&
+					rm -rf #{release_path}/frontend/files/#{folder} &&
+					ln -s #{shared_path}/files/#{folder} #{release_path}/frontend/files/#{folder}
 				}
 			end
 		end	
