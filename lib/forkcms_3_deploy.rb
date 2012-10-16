c3a1fae1d63b87289fcc02e112a6aa9c604bcee0
Tijs Verkoyen <tijs@verkoyen.eu>
2012-10-16 16:58:31 +0200

    Added warning so people can start using forkcms_deploy

--- lib/forkcms_3_deploy.rb
+++ lib/forkcms_3_deploy.rb
@@ -1,2 +1,4 @@
-require 'forkcms_3_deploy/forkcms'
-require 'forkcms_3_deploy/overwrites'
+puts "This gem will not be maintained anymore, use the forkcms_deploy-gem"
+puts "The forkcms_deploy-gem is updated and able to deal with all previous"
+puts "and newer versions of Fork CMS"
+exit
